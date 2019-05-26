e636d8aa0d7ec2da736a1b84de4c79c622c24c09
Mr.Su <shuyuncong@gmail.com>
2019-05-26 19:37:06 +0800

    1、新增加密解密

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <x-header>123</x-header>
+    <router-link to="/index">One</router-link>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
