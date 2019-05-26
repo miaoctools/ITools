3865c264bdf43305cb328c03d8167501e49d201b
Mr.Su <shuyuncong@gmail.com>
2019-05-26 10:50:20 +0800

    测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <x-header>123</x-header>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -8,10 +9,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { XHeader } from 'vux'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    XHeader
   }
 }
 </script>
