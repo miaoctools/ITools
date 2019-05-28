b1b823c0f666285ab30125748e18705dab561330
zhaoyang <675175681@qq.com>
2019-05-28 19:50:48 +0800

    首页框子+安装less

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,57 @@
 <template>
   <div class="home">
-    <x-header>123</x-header>
+<!-- 
     <router-link to="/index">One</router-link>
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <x-header>iTools</x-header>
+    <tabbar>
+      <tabbar-item selected >
+        <img slot="icon" src="../assets/img/detail.svg">
+        <span slot="label">iTools</span>
+      </tabbar-item>
+      <tabbar-item>
+        <img slot="icon" src="../assets/img/person.svg">
+        <span slot="label">detail</span>
+      </tabbar-item>
+    </tabbar>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import { XHeader } from 'vux'
+/* import HelloWorld from '@/components/HelloWorld.vue' */
+import { XHeader, Tabbar, TabbarItem } from 'vux'
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Tabbar,
+    TabbarItem,
+/*     HelloWorld, */
     XHeader
   }
 }
 </script>
+<style lang="scss">
+$backColor: #49AAEE;
+$headFontColor:#ffffff;
+.home{
+  /*--------------修改标签样式---------------*/
+  p{
+    margin-block-start: 0.5em;
+    margin-block-end: 0.5em;
+  }
+  /*-------------修改vux封装样式------------*/
+  .vux-header{
+    background-color: $backColor;
+  }
+  .vux-header .vux-header-left a{
+    color: $headFontColor;
+  }
+  .vux-header .vux-header-left .left-arrow:before{
+    border-color: $headFontColor;
+  }
+}
+</style>
+
+
